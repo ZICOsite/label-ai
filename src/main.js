27afc7418444bbd633c940e950beee436c33bfc5
ZICO <ziyodahmedov23@gmail.com>
2024-06-17 00:40:18 +0500

    change scroll animation

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import './assets/styles/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const lenis = new Lenis()
+// const lenis = new Lenis()
 
-lenis.on('scroll', (e) => {
-  // console.log(e)
-})
+// lenis.on('scroll', (e) => {
+//   // console.log(e)
+// })
 
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
 
-requestAnimationFrame(raf)
+// requestAnimationFrame(raf)
 
 createApp(App).mount('#zico')
